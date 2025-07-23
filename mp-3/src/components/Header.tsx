7871a6c478e76cdeda6eb75d45d2c53a9cd22f43
Yazan Alzahrany <yazan@bu.edu>
2025-07-22 22:00:45 -0400

    resovled prop issue

--- mp-3/src/components/Header.tsx
+++ mp-3/src/components/Header.tsx
@@ -3,40 +3,16 @@ import { Link } from 'react-router-dom';
 import owl from '../assets/owl.png';
 
 const StyledHeader = styled.header`
-  display: flex;
-  align-items: center;
-
-  /* Owl image styling */
-  #owl {
-    width: 5vw;
-    min-width: 70px;
-    height: auto;
-    margin: 1vw 2vw;
-  }
-
-  /* Heading styles */
-  h1 {
-    color: #3B9C9C;
-    font-family: Garamond, serif;
-    font-weight: bold;
-      font-size: calc(18px + 2vw);
-      margin: 0;
-  }
-
-  /* Link styles */
-  a {
-    color: #3B9C9C;
-    text-decoration: none;
-    &:hover {
-      color: #E85F5F;
-      text-decoration: none;
-    }
-  }
+    /* …styles… */
 `;
 
-// Header component
+/* --- prop type --- */
+interface HeaderProps {
+    title: string;
+    to?: string;
+}
 
-export default function Header({ title, to = '/' }) {
+export default function Header({ title, to = '/' }: HeaderProps) {
     return (
         <StyledHeader>
             <img id="owl" src={owl} alt="Logo" />
